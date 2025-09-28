28318efee8021c8e03f8fdf1c9c1bd964404df58
Madhav <madhavraj1212@gmail.com>
2025-09-28 19:19:02 +0530

    fixed login bug and now shows you the list of songs before downloading

--- Main.py
+++ Main.py
@@ -2,7 +2,8 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import yt_dlp
 import os
-#version 1.1.0
+from prettytable import PrettyTable
+#version 1.2.0
 
 
 
@@ -44,55 +45,73 @@ def spotify_playlist(link):
 
         offset += limit
 
+    table=PrettyTable()
+    table.field_names=["Index","Title","Artist"]
+    for i in range(len(songs)):
+        table.add_row([i+1,songs[i][0],songs[i][1]])
 
-    return songs
+    print(table)
+
+
+    cont=input("Continue with the downloading? [Y/n]\n==>")
+    if cont.lower()=='y':
+        return songs
+    else:
+        print("Returning to main menu...")
+        Main()
 
 def Playlist_Downloader(Songs):
-    for index,song in enumerate(Songs):
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'noplaylist': True,  
-            'outtmpl': 'Downloads/%(title)s.%(ext)s',  
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',  
-            }]
-        }
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([f"ytsearch1:{song[0]} {song[1]} lyrics"])
-
-
+    try:
+        for index,song in enumerate(Songs):
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'noplaylist': True,  
+                'outtmpl': 'Downloads/%(title)s.%(ext)s',  
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',  
+                }]
+            }
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([f"ytsearch1:{song[0]} {song[1]} lyrics"])
+    except :
+        print("Inputed link is not supported, make sure its a public playlist only")
 
 
 
 def Main():
     if os.path.isfile("credentials.txt"):
-        
-        choice=int(input("[1] Download the playlist\n[2] Re-enter the credentials\n==>"))
-        if choice == 1:
-            link=input("Paste the link of the playlist (MUST BE A PUBLIC PLAYLIST):\n==>")
-            Songs=spotify_playlist(link);    
-            Playlist_Downloader(Songs);
-        elif choice ==2:
-            with open("credentials.txt","w") as f:
-                id=input("Enter your Spotify Client ID:\n==>")
-                f.write(id)
-                s_id=input("Enter your client secret ID:\n==>")
-                f.write(s_id)
+        try:
+            choice=int(input("[1] Download the playlist\n[2] Re-enter the credentials\n[0] Exit\n==>"))
+            if choice == 1:
+                link=input("Paste the link of the playlist (MUST BE A PUBLIC PLAYLIST):\n==>")
+                Songs=spotify_playlist(link);    
+                Playlist_Downloader(Songs);
+            elif choice ==2:
+                with open("credentials.txt","w") as f:
+                    id=input("Enter your Spotify Client ID:\n==>")
+                    f.writelines(str(id)+"\n")
+                    s_id=input("Enter your client secret ID:\n==>")
+                    f.writelines(str(s_id)+"\n")
+                Main()
+            elif choice==0:
+                print("Exiting...")
+            else:
+                print("Please enter a valid option")
+                Main()
+        except:
             Main()
-        
-        else:
-            print("Please enter a valid option")
-            Main()
+
+            
 
         
     else:
         with open("credentials.txt","w") as f:
             id=input("Enter your Spotify Client ID:\n==>")
-            f.write(id)
+            f.writelines(str(id)+"\n")
             s_id=input("Enter your client secret ID:\n==>")
-            f.write(s_id)
+            f.writelines(str(s_id)+"\n")
         Main()
 
 Main()
